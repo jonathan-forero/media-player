f14fcbb7aded578317fd8473743c007b1fe6a201
forerojonathan <forerojonathan@gmail.com>
2020-11-25 20:17:37 -0500

    add: service worker

--- assets/index.js
+++ assets/index.js
@@ -13,3 +13,9 @@ playBtn.onclick = () => player.tooglePlay();
 
 const muteBtn = document.querySelector('#muteBtn');
 muteBtn.onclick = () => player.toogleMute();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  });
+}
