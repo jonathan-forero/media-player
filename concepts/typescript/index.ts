a420bb766b5f48b09f002c8f7a9871f466477c89
forerojonathan <forerojonathan@gmail.com>
2020-12-09 19:09:49 -0500

    add: Typescript basic types

--- concepts/typescript/index.ts
+++ concepts/typescript/index.ts
@@ -1,7 +1,37 @@
-console.log('Hello, typescript');
+// Boolean
+let muted: boolean = true;
+muted = false;
+// muted = 'muted'; -> Type 'string' is not assignable to type 'boolean'
 
-function add(a: number, b: number) {
-  return a + b;
+// Number
+let numberA: number = 42;
+let numberB: number = 6;
+let result = numberA / numberB;
+
+// String
+let myName: string = 'Tatán';
+let greeting = `My name is ${myName}`;
+
+// Arrays
+let people: string[] = [];
+people = ['Keanu', 'Chuck', 'Tom'];
+// people.push(100); -> Argument of type 'number' is not assignable to parameter of type 'string'
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Tatán');
+peopleAndNumbers.push(100);
+
+// Enums
+enum Color {
+  Red = 'Red',
+  Green = 'Green',
+  Blue = 'Blue',
 }
+let favColor: Color = Color.Red;
+console.log(`My Fav color is ${favColor}`);
 
-const sum = add(2, 4);
+// Any
+let joker: any = 'Joker';
+joker = { type: 'Wildcard' };
+
+// Object
+let someObject: Object = { type: 'WildCard' };
